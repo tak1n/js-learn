032e7bad63a74e96b4efe31b289ef6bbd2700df0
Benjamin <benny.klotz92@gmail.com>
2021-06-18 23:01:38 +0200

    Try out express with typescript

---
+++ typescript-express/main.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+app.get('/', (_req,res) => res.send('Express + TypeScript Server, is cool'));
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
