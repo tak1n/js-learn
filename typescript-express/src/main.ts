5826070e052dbf12438c2bb0fee0e71bb95efb4d
Benjamin <benny.klotz92@gmail.com>
2021-06-19 01:49:15 +0200

    Fix main.ts

--- typescript-express/src/main.ts
+++ typescript-express/src/main.ts
@@ -1,11 +1,7 @@
 import { createApp } from './app'
 
-createApp()
-  .then((server: { listen: (arg0: number, arg1: () => void) => void }) => {
-    server.listen(3000, () => {
-      console.info(`Listening on http://localhost:3000`)
-    })
-  })
-  .catch((err: any) => {
-    console.error(`Error: ${err}`)
-  })
+const port = process.env.PORT || 3000
+
+createApp().listen(port, () => {
+  console.info(`Listening on Port ${port}`);
+});
