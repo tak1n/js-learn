0c01ac37cff32f81139e34ca3a3b4546d6ccf3a9
Benjamin <benny.klotz92@gmail.com>
2021-06-21 22:15:39 +0200

    Try out fastify

---
+++ fastify/src/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const fastify = require('fastify');
+const PORT = process.env.PORT || 3000;
+
+const build = (opts={}) => {
+    const app = fastify(opts);
+
+    app.get('/', async (request, reply) => {
+        return { message: 'Hello World!!' };
+    })
+
+    return app;
+}
+ 
+module.exports = build;
---
+++ fastify/test/app.test.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const build = require('../src/app.js');
+const request = require('supertest');
+
+const app = build();
+
+describe('App', () => {
+  beforeAll(async () => {
+      await app.ready();
+  });
+
+  afterAll(() => {
+    app.close();
+  });
+
+  describe('GET / without supertest', () => {
+    it('should respond with a 200 response and a \'Hello World\' body', async () => {
+      const { statusCode, body } = await app.inject({
+        method: 'GET',
+        url: '/'
+      });
+
+      expect(statusCode).toBe(200);
+      expect(JSON.parse(body)).toStrictEqual({ message: 'Hello World!!' });
+    });
+  });
+
+  describe('GET /', () => {
+    it('should respond with a 200 response and a \'Hello World\' body', async () => {
+      const { status, body } = await request(app.server).get('/')
+                                    
+      expect(status).toBe(200);
+      expect(body).toStrictEqual({ message: 'Hello World!!' });
+    });
+  });
+});
