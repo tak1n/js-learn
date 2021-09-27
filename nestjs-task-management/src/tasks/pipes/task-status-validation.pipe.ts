0a0378cec85d9b15d737ff076a024db1e98660ba
Benjamin <benny.klotz92@gmail.com>
2021-09-27 23:06:13 +0200

    Adapt validation pipe

--- nestjs-task-management/src/tasks/pipes/task-status-validation.pipe.ts
+++ nestjs-task-management/src/tasks/pipes/task-status-validation.pipe.ts
@@ -1,15 +1,20 @@
 import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { TaskStatus } from '../task.model';
 
 export class TaskStatusValidationPipe implements PipeTransform {
-  readonly allowedStatuses = ['OPEN', 'IN_PROGRESS', 'DONE'];
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
 
   transform(value: any) {
     value = value.toUpperCase();
 
-    if (this.allowedStatuses.includes(value)) {
-      return value;
-    } else {
+    if (!this.allowedStatuses.includes(value)) {
       throw new BadRequestException(`Invalid status "${value}"`);
     }
+
+    return value;
   }
 }
