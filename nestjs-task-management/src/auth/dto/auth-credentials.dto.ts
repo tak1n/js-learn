57c189f399d3bad7671f87c7d0e489e0e388de0c
Benjamin <benny.klotz92@gmail.com>
2021-10-04 20:15:57 +0200

    Password validation

--- nestjs-task-management/src/auth/dto/auth-credentials.dto.ts
+++ nestjs-task-management/src/auth/dto/auth-credentials.dto.ts
@@ -1,5 +1,22 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
 
+  @IsString()
+  @MinLength(8)
+  @Matches(/((?=.*\d)|(=?.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must contain uppercase letters, lowercase letters and special characters',
+  })
   password: string;
 }
