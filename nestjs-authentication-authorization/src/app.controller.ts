8818ee8e3c8314b0cd25c6ba7c660da26185b2d5
Benjamin <benny.klotz92@gmail.com>
2021-10-19 19:43:00 +0200

    Try out local authentication strategy with session storage

--- nestjs-authentication-authorization/src/app.controller.ts
+++ nestjs-authentication-authorization/src/app.controller.ts
@@ -1,19 +1,22 @@
 import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthenticatedGuard } from './auth/authenticated.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(AuthenticatedGuard)
   @Get('protected')
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Req() req: any): string {
+    // return this.appService.getHello();
+    return req.user;
   }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Req() req: any): any {
-    return req.user;
+  login(): any {
+    return { message: 'Logged in!' };
   }
 }
